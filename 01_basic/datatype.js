2ece03ee77f69e7e78751947750a90464f8b8e84
vikas kumar <insphere.vikaskumar@gmail.com>
2024-10-02 17:53:04 +0530

    dattype chage

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -1,6 +1,35 @@
 "use strict"; // treat all JS code as newer version
 
 //number
-let age = 2
+let age = 2  // 2 to power 53
 console.log(age)
 console.log(typeof age);
+
+//bigint
+//string
+let nameOfCoustomer = "vikas" //'vikas'//`vikas`
+console.log(nameOfCoustomer)
+console.log(typeof nameOfCoustomer);
+
+//boolean
+let costomerOpen = true; // value true or false;
+console.log(costomerOpen)
+console.log(typeof costomerOpen);
+
+
+// null => standalone value
+let costomerState = null;
+console.log(typeof costomerState);
+// undefined => 
+let costomerCity;
+console.log(typeof costomerCity);
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object not "null" for legacy reasons
+
+
