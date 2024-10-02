be9499124e61cd295285a54979d445e1ffa0afc7
vikas kumar <insphere.vikaskumar@gmail.com>
2024-10-02 17:10:27 +0530

    change

--- 01_basic/variables.js
+++ 01_basic/variables.js
@@ -10,7 +10,8 @@ because of issue in block scope and functional scope
 accountPass ="Vikas12345"
 accountCity ="Delhi"
 
-// accountId = 2 // not allowed because it's const variables let change 
+// accountId = 2 // 
+//not allowed because it's const variables let change 
 let accountState;
 console.log(accountCity);
 console.table([accountId, accountName, accountCity, accountPass, accountState]);
