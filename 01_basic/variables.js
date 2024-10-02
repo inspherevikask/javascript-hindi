1e2142b318d80fe6e0bbf6c915ad6d1f2609b322
vikas kumar <insphere.vikaskumar@gmail.com>
2024-10-02 16:18:00 +0530

    variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,22 @@
+const accountId = 12345
+let accountName = "vikas"
+var accountCity ="Jaipur"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+accountPass ="Vikas12345"
+accountCity ="Delhi"
+
+// accountId = 2 // not allowed because it's const variables let change 
+let accountState;
+console.log(accountCity);
+console.table([accountId, accountName, accountCity, accountPass, accountState]);
+
+
+
+
+
+
