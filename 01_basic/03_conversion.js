1f18e1691694c41761b182e0e801130d9515c682
vikas kumar <insphere.vikaskumar@gmail.com>
2024-10-03 17:14:30 +0530

    change update

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,33 @@
+
+// *********************** Number Conversion ***********************
+
+let score = undefined
+// console.log(typeof score)
+console.log(typeof(score))
+let scoreChangeInNumber = Number(score)
+console.log(scoreChangeInNumber)
+console.log(typeof scoreChangeInNumber)
+
+// "33" => 33 
+// "33abc" => NaN  // Not a Number
+//  true => 1; false => 0
+// null => 0
+// undefined  => NaN
+
+
+
+// *********************** Boolean Conversion ***********************
+let logIN = 0;
+console.log(typeof(logIN));
+let chagelogInboolean = Boolean(logIN);
+console.log(chagelogInboolean);
+console.log(typeof(chagelogInboolean));
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// *********************** String Conversion ***********************
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
