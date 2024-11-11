dd536c177c3ad59e44b355b9e501bc02e1b18b62
vikas kumar <insphere.vikaskumar@gmail.com>
2024-11-11 11:55:30 +0530

    update

---
+++ 01_basic/05_operators.js
@@ -0,0 +1,48 @@
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Bitwise Operators
+// String Operators
+// Other Operators
+
+
+
+
+// .............Arithmetic Operators.....................
+//+ ==> console.log(5 + 2);
+//- ==> console.log(5 - 2);
+// / ==> console.log(10 / 2);
+// * ==> console.log(2 * 2 );
+// ** ==>  console.log(2 ** 2 ); // Result == 2 x 2 = 4  
+           //console.log(2 ** 3 ); // Result == 2 x 2 x 2  = 8
+ 
+ console.log(1200 - (20/100*1200)) ; 
+
+// console.log(5 / 2);
+// console.log(Math.round(5 / 2));
+let currentPrice = 5999;
+let offerPrice = 30;
+
+let currentValue = currentPrice - 30/100*currentPrice
+// console.log(valuewithoffer);
+console.log(currentValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
